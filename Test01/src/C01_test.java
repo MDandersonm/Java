853d3f9d7e810c15ccd6574cf77e835e5a3497d4
MDandersonm <1215kimyj@gmail.com>
2023-03-22 21:21:49 +0900

    test:[김영진] optional테스트

--- Test01/src/C01_test.java
+++ Test01/src/C01_test.java
@@ -5,6 +5,7 @@ public class C01_test {
 	public static void main(String[] args) {
 		String str= "hi";
 		Optional<String> op =null;
+		System.out.println(op);
 		System.out.println(op.get());
 
 	}
